2c088ecaebbb66a82251158a35e30d9ff919448a
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-04 01:56:59 +0530

    first commit

---
+++ Backend/middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const authMiddleware = (req , res , next) => {
+    try {
+        const token = req.header("auth-token");
+
+    if(!token){
+        return res.set(401).json({error: "Unauthorized"});
+    }
+
+    const user = jwt.verify(token, process.env.JWT_SECRET)
+    if(!user){
+        return res.status(401).json({error: 'Unauthorized'})
+    }
+    req.user = user;
+    } catch (error) {
+        return res.status(401).json({error: 'Unauthorized'})
+    }
+    
+}
