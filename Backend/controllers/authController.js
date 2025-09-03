40228468c94eb211578292650674496f32cb9882
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-04 01:20:26 +0530

    first commit

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,6 +1,6 @@
 import { prismaClient } from "../outes/index.js";
 import { userSchema } from "../schema/user.js";
-import { hashSync } from "bcryptjs";
+import { compare, compareSync, hashSync } from "bcryptjs";
 
 
 export const RegisterController = async(req , res) =>{
@@ -25,22 +25,28 @@ export const RegisterController = async(req , res) =>{
 };
 
 export const LoginController = async(req , res) =>{
-    const userData = userSchema.parse(req.body)
+    const userData = EmailLoginSchema.parse(req.body)
 
-    if(!userData.name || !userData.email || !userData.password || !userData.phoneNo){
+    if(!userData.email || !userData.password){
         return res.status(400).json({ message: "Please fill all the feilds"});
     }
 
-    const hashPassword = await hashSync(userData.password, 10);
+    const user = await prismaClient.user.findUnique({
 
-    const user = await prismaClient.user.create({
-        data: {
-            name: userData.name,
-            email:userData.email,
-            password: hashPassword,
-            phoneNo: userData.phoneNo
+        where: {
+            email: userData.email
         }
     })
 
-    return res.status(201).json({ message: "User create successfully",user})
+    if(!user){
+        return res.status(404).json({message: "User not found"})
+    }
+
+    const isPasswordMatch = await compareSync(userData.password , user.password);
+
+    if(!isPasswordMatch){
+        return res.status(401).json({ message: "User created successfully",user})
+    }
+
+    return res.status(200).json({ message: "login successfully",user})
 }
