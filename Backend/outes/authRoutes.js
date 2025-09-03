781eb81165f847950db0f46566abb4b2209c3316
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-04 00:18:07 +0530

    first commit

---
+++ Backend/outes/authRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { RegisterController } from "../controllers/authController.js";
+
+const authRouter = express.Router();
+
+authRouter.post('/register',RegisterController)
+
+
+export default authRouter;
---
+++ Backend/outes/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import authRouter from "./authRoutes.js";
+
+const rootRouter = express.Router();
+
+rootRouter.use('/auth',authRouter)
+
+export default rootRouter;
