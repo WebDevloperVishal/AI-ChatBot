1be56c4409f652cc70f0539c37879c2bf733c1ef
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-04 19:40:42 +0530

    first commit

--- Backend/outes/authRoutes.js
+++ Backend/outes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { LoginController, RegisterController } from "../controllers/authController.js";
+import { GetUserContoller } from "../middlewares/authMiddleware.js";
 
 const authRouter = express.Router();
 
 authRouter.post('/register',RegisterController);
 authRouter.post('/login',LoginController);
+authRouter.get("/getUser",GetUserContoller)
 
 
 export default authRouter;
