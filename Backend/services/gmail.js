d2bdf392d11f8270a79e4c67ee11641d5ace4a43
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-05 00:58:29 +0530

    first commit

--- Backend/services/gmail.js
+++ Backend/services/gmail.js
@@ -1,1 +1,17 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+ const MAIL_USER = process.env.MAIL_USER
+ const MAIL_PASSWORD = process.env.MAIL_PASSWORD
+
+export const SendEmail = async ({to,subject,html})=>{
+    const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: MAIL_USER,
+      pass: MAIL_PASSWORD,
+    },
+});
+
+}
