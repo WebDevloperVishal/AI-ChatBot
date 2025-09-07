537c4e695e1d39ffceee02b07f1c427e8a774a42
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-07 15:39:40 +0530

    first commit

--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -1,23 +1,25 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import { UnauthorizedExecption } from "../exceptions/unauthorized.js";
+import { ErrorCodes } from "../exceptions/root.js";
 dotenv.config();
 
-export const authMiddleware = (req , res , next) => {
+export const authMiddleware = (req, res, next) => {
     try {
         const token = req.header("auth-token");
 
-    if(!token){
-        return res.set(401).json({error: "Unauthorized"});
+        if (!token) {
+            return next(new UnauthorizedExecption("Unauthorized", ErrorCodes.UNAUTHORIZED))
+        }
+
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+        if (!user) {
+            return next(new UnauthorizedExecption("Unauthorized", ErrorCodes.UNAUTHORIZED))
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        return next(new UnauthorizedExecption("Unauthorized", ErrorCodes.UNAUTHORIZED))
     }
 
-    const user = jwt.verify(token, process.env.JWT_SECRET)
-    if(!user){
-        return res.status(401).json({error: 'Unauthorized'})
-    }
-    req.user = user;
-    next();
-    } catch (error) {
-        return res.status(401).json({error: 'Unauthorized'})
-    }
- 
 }
